065e914c146eebb546ae1176cbb443857164b96d
jayaskanhaiya0001 <jayaskanhaiya0001@gmail.com>
2023-03-30 20:45:03 +0530

    Ass 1 Add Header

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
+import Header from "./components/Layout/header";
 import Products from "./components/Products/Products";
 function App() {
   return (
     <>
+      <Header />
       <Products />
     </>
   );
